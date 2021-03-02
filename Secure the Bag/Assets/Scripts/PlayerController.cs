f301f2b2e80b8c93a745c1783491dfe70d5b3909
Jack-DLC <45299615+Jack-DLC@users.noreply.github.com>
2021-03-02 14:54:19 -0500

    Changed Scene Name
    Add a home screen for the final product

--- Secure the Bag/Assets/Scripts/PlayerController.cs
+++ Secure the Bag/Assets/Scripts/PlayerController.cs
@@ -8,59 +8,83 @@ public class PlayerController : MonoBehaviour
     private CharacterController characterController;
     private Animator animator;
 
-    [SerializeField]
-    private Vector3 Xposition;
-    public float movementSpeed, Xincrement, jumpSpeed, gravity;
+    public int laneNumber = 0;
+    public int lanesCount = 2;
 
-    private Vector3 movementDirection = Vector3.zero;
+    bool didChangeLastFrame = false;
+
+    // These variables deal with lane position and change
+    public float laneDistance = 10; // dist. between lanes
+    public float firstLaneXPos = 0; // the x coordinate of the starting lane
+    public float deadZone = 0.1f; // if a value is greater than this the player can perform an action
+    public float sideSpeed = 5; // how quickly a player changes lanes
+
+    // these variables hand jumping
+    public float jumpHeight = 2.5f; //how high the player jumps
+    public float jumpForce = 5; // how quickly they jump
+    public float gravity; // the force pulling them to the ground
+ 
+
+    public Vector3 objectVelocity; // this will handle how fast objects move
+    public Vector3 jump; //
+
+    private Rigidbody rigg;
 
 
-    
+
     void Start() // Start is called before the first frame update
     {
+
+        // This controls the character animations
         characterController = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
+        rigg = GetComponent<Rigidbody>();
+        jump = new Vector3(0f, jumpHeight, 0f);
+
     }
 
-    
+
     void Update() // Update is called once per frame
     {
-        // Vector2.MoveTowards() moves the player towards certain position instead of teleporting them there
-        // Time.delteTime uses the time of the players computer to sync events. This reduces some performance issues on less poerful computers
-        transform.position = Vector3.MoveTowards(transform.position, Xposition, movementSpeed * Time.deltaTime);
+        Vector3 currentPosition = transform.position; //the current position of the player 
 
-        if (Input.GetKeyDown(KeyCode.UpArrow)) // if player has hit 'A' the Character moves left Xincrement units
+        //add a gui element to start the game whena button is pressed then begin the animations
+        animator.SetBool("isRunning", Input.GetAxisRaw("Vertical") != 0); // running animation runs when the player starts playing
+
+        //"Horizontal" is a default input axis set to arrow keys and A/D
+        //We want to check whether it is less than the deadZone instead of whether it's equal to zero 
+        float changeLanes = Input.GetAxis("Horizontal");
+
+        if (Mathf.Abs(changeLanes) > deadZone)
         {
-            Xposition = new Vector3(transform.position.x + Xincrement, transform.position.z, transform.position.y);
-            transform.position = Xposition;
+            if (!didChangeLastFrame)
+            {
+                didChangeLastFrame = true; //Prevent overshooting lanes
+                laneNumber += Mathf.RoundToInt(Mathf.Sign(changeLanes));
+                if (laneNumber < -1) laneNumber = -1;
+                else if (laneNumber >= lanesCount) laneNumber = lanesCount - 1;
+            }
         }
-        else if (Input.GetKeyDown(KeyCode.DownArrow)) // if player hits 'S' the character moves left -Xincrement units
+        else
         {
-            Xposition = new Vector3(transform.position.x - Xincrement, transform.position.z, transform.position.y);
-            transform.position = Xposition;
-        }
-
-
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        characterController.Move(move * Time.deltaTime * movementSpeed);
-        
-        //An alternative movement mechanic
-        //Vector3 inputMovement = transform.forward * movementSpeed * Input.GetAxisRaw("Vertical");
-        //characterController.Move(inputMovement * Time.deltaTime);
-
-        // This controlls jumping
-        if(Input.GetButton("Jump") && characterController.isGrounded)
-        {
-            movementDirection.y = jumpSpeed;
+            didChangeLastFrame = false;
+            //The user hasn't pressed a direction this frame, so allow changing directions next frame.
         }
 
-        movementDirection.y -= gravity * Time.deltaTime;
+        float jumpKey = Input.GetAxis("Vertical");
 
-        characterController.Move(movementDirection * Time.deltaTime);
+        if (Mathf.Abs(jumpKey) > deadZone && rigg.velocity.y == 0)
+        {
+            //animator.SetBool("isJumping", !characterController.isGrounded);
+
+            rigg.AddForce(jump * jumpForce, ForceMode.Impulse);
+            //Debug.Log(test);
+        }
 
 
-        // Controlls Animaition
-        animator.SetBool("isRunning", Input.GetAxisRaw("Vertical") != 0);
-        animator.SetBool("isJumping", !characterController.isGrounded);
+        currentPosition.x = Mathf.Lerp(currentPosition.x, firstLaneXPos + laneDistance * laneNumber, Time.deltaTime * sideSpeed);
+        transform.position = currentPosition;
+
+
     }
 }
