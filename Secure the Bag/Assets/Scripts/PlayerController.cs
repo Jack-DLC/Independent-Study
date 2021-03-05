ff57d9b6ceb6f5948656da0d5c089afc8f00b7e2
Jack-DLC <45299615+Jack-DLC@users.noreply.github.com>
2021-03-05 16:44:58 -0500

    Grids Spawn and player runs
    Created a grid prefab with a grid spawner, When the player collides with the spawner a new grid is created behind the current one.

--- Secure the Bag/Assets/Scripts/PlayerController.cs
+++ Secure the Bag/Assets/Scripts/PlayerController.cs
@@ -5,86 +5,93 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController), typeof(Animator))]
 public class PlayerController : MonoBehaviour
 {
-    private CharacterController characterController;
-    private Animator animator;
+    private CharacterController characterController; // used to move the player 
+    private Animator animator; // used to handle animations
+    private Rigidbody rigg; // A reference to the rigidbody of the player
 
-    public int laneNumber = 0;
-    public int lanesCount = 2;
+    public GameObject[] prefab; // used to spawn in tile prefabs
+
+
 
     bool didChangeLastFrame = false;
+    
+
+    public float jumpHeight = 2.5f; //how high the player jumps
+    public float jumpForce = 5; // how quickly they jump
+    public float gravity; // the force pulling them to the ground
 
     // These variables deal with lane position and change
+    public int laneNumber = 0; // the lane the player is currently
+    public int lanesCount = 2; // there are actually 
     public float laneDistance = 10; // dist. between lanes
     public float firstLaneXPos = 0; // the x coordinate of the starting lane
     public float deadZone = 0.1f; // if a value is greater than this the player can perform an action
     public float sideSpeed = 5; // how quickly a player changes lanes
+    public float runningSpeed; // how quickly a player changes lanes     
 
-    // these variables hand jumping
-    public float jumpHeight = 2.5f; //how high the player jumps
-    public float jumpForce = 5; // how quickly they jump
-    public float gravity; // the force pulling them to the ground
- 
-
-    public Vector3 objectVelocity; // this will handle how fast objects move
-    public Vector3 jump; //
-
-    private Rigidbody rigg;
-
-
+    public float gridSpawnCoords = 80.0f;
+    public float gridOffset = 80;
 
     void Start() // Start is called before the first frame update
     {
 
         // This controls the character animations
-        characterController = GetComponent<CharacterController>();
-        animator = GetComponent<Animator>();
-        rigg = GetComponent<Rigidbody>();
-        jump = new Vector3(0f, jumpHeight, 0f);
-
+        characterController = GetComponent<CharacterController>(); // used for animations
+        animator = GetComponent<Animator>(); // used for setting the animations
+        rigg = GetComponent<Rigidbody>(); // Not sure if i need this anymore
     }
 
 
-    void Update() // Update is called once per frame
+    void Update()
     {
-        Vector3 currentPosition = transform.position; //the current position of the player 
-
-        //add a gui element to start the game whena button is pressed then begin the animations
-        animator.SetBool("isRunning", Input.GetAxisRaw("Vertical") != 0); // running animation runs when the player starts playing
-
         //"Horizontal" is a default input axis set to arrow keys and A/D
         //We want to check whether it is less than the deadZone instead of whether it's equal to zero 
-        float changeLanes = Input.GetAxis("Horizontal");
+        float input = Input.GetAxis("Horizontal");
 
-        if (Mathf.Abs(changeLanes) > deadZone)
+        // if the player hits "a", "d", left arrow, or right arrow the character will change lanes
+        if (Mathf.Abs(input) > deadZone)
         {
             if (!didChangeLastFrame)
             {
                 didChangeLastFrame = true; //Prevent overshooting lanes
-                laneNumber += Mathf.RoundToInt(Mathf.Sign(changeLanes));
-                if (laneNumber < -1) laneNumber = -1;
+                laneNumber += Mathf.RoundToInt(Mathf.Sign(input));
+                if (laneNumber < 0) laneNumber = 0;
                 else if (laneNumber >= lanesCount) laneNumber = lanesCount - 1;
             }
         }
+        
         else
         {
             didChangeLastFrame = false;
             //The user hasn't pressed a direction this frame, so allow changing directions next frame.
         }
+        
 
-        float jumpKey = Input.GetAxis("Vertical");
-
-        if (Mathf.Abs(jumpKey) > deadZone && rigg.velocity.y == 0)
+        
+        if (Input.GetAxis("Horizontal") > deadZone)
         {
-            //animator.SetBool("isJumping", !characterController.isGrounded);
-
-            rigg.AddForce(jump * jumpForce, ForceMode.Impulse);
-            //Debug.Log(test);
+            rigg.AddForce(0, jumpForce, 0);
         }
+        
 
-
-        currentPosition.x = Mathf.Lerp(currentPosition.x, firstLaneXPos + laneDistance * laneNumber, Time.deltaTime * sideSpeed);
-        transform.position = currentPosition;
-
-
+        rigg.velocity = new Vector3(0, 0, runningSpeed);// jumping
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Lerp(pos.x, firstLaneXPos + laneDistance * laneNumber, Time.deltaTime * sideSpeed);
+        transform.position = pos;
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        bool has_triggered = false;
+        if (other.gameObject.tag == "SpawnPoint" && has_triggered == false)
+        {
+            has_triggered = true;
+            Debug.Log("player");
+            Instantiate(prefab[0], new Vector3(0, 0, gridSpawnCoords), Quaternion.identity);
+            gridSpawnCoords += gridOffset;
+        }
+        
+    }
+    
+
 }
