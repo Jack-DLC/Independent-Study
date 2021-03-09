9e763343b1db9d5a6adbcd206ba0c8e221d577a0
Jack-DLC <45299615+Jack-DLC@users.noreply.github.com>
2021-03-09 15:08:44 -0500

    Obstacles Spawn in center lane

---
+++ Secure the Bag/Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    private GameObject playerPosition;
+    private Vector3 cameraPosition;
+    //private Vector3 cameraOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerPosition = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Rotate(new Vector3(16, 0, 0));
+        //Vector3 cameraOffset = new Vector3(0, 5, playerPosition.transform.position.z);
+        //transform.LookAt(cameraPosition);
+        
+
+
+        cameraPosition.y = 8;
+        cameraPosition.z = playerPosition.transform.position.z - 14;
+        transform.position = cameraPosition;
+    }
+}
--- Secure the Bag/Assets/Scripts/tileManager.cs
+++ Secure the Bag/Assets/Scripts/tileManager.cs
@@ -6,13 +6,27 @@ public class tileManager : MonoBehaviour
 {
 
     public GameObject[] prefab; // used to spawn in tile prefabs
+    public GameObject grid; // a gameObject used to save newly instantiated grids to a list of all grids in the scene
 
-    public float gridSpawnCoords = 80.0f;
-    public float gridOffset = 80;
+    // activeGrids is a list of GameObjects that contains references to all grid prefabs  that have been spawned into the scene
+    public List<GameObject> activeGrids = new List<GameObject>();
+    // activeObstacles is a list of GameObjectsthat contains references to all grid prefabs  that have been spawned into the scene
+    public List<GameObject> activeObstacles = new List<GameObject>();
+
+    public float gridSpawnCoords = 80.0f; // the z coordinate that new tiles will spawn on
+    public float gridOffset = 80; // spawning offset distance
 
     // Start is called before the first frame update
     void Start()
     {
+        GameObject grid;
+        grid = Instantiate(prefab[1], new Vector3(0, 0, 10), Quaternion.identity);
+        activeGrids.Add(grid);
+        grid = Instantiate(prefab[0], new Vector3(0, 0, gridSpawnCoords), Quaternion.identity);
+        obstacleSpawner();
+        gridSpawnCoords += gridOffset;
+        activeGrids.Add(grid);
+
         
     }
 
@@ -24,14 +38,43 @@ public class tileManager : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        bool has_triggered = false;
-        if (other.gameObject.tag == "SpawnPoint" && has_triggered == false)
+        //GameObject grid;
+        //bool has_triggered = false;
+
+        if (other.gameObject.tag == "SpawnPoint" )
         {
-            has_triggered = true;
-            Debug.Log("player");
-            Instantiate(prefab[0], new Vector3(0, 0, gridSpawnCoords), Quaternion.identity);
-            gridSpawnCoords += gridOffset;
+            //has_triggered = true;
+            //Debug.Log("player");
+
+            gridSpawner();
+            obstacleSpawner();
         }
 
+    }
+
+    private void destroyGrid()
+    {
+        Destroy(activeGrids[0]);
+        activeGrids.RemoveAt(0);
+    }
+
+    private void gridSpawner() // creates a grid adn populates it with obstacles
+    {
+        GameObject grid;
+        grid = Instantiate(prefab[0], new Vector3(0, 0, gridSpawnCoords), Quaternion.identity);
+        gridSpawnCoords += gridOffset;
+        activeGrids.Add(grid);
+        destroyGrid();
+    }
+
+    private void obstacleSpawner() // controls logic behind obstacle spawning
+    {
+        GameObject obstacles;
+        for (float i = gridSpawnCoords - 10; i < gridSpawnCoords + 70; i += 10)
+        {
+            obstacles = Instantiate(prefab[3], new Vector3(0, 0, i), Quaternion.Euler(0, 90, 0));
+            activeObstacles.Add(obstacles);
+
+        }
     }
 }
